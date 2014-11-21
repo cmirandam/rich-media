17db53e8d193b1c4a8169f21ce2c412d0d72ef3d
Carola Miranda <carola@carola-Inspiron-N4050>
2014-11-21 12:45:08 -0300

    Nuevo repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RichMedia::Application.config.secret_token = '555edcae80ff23763563a1c23d61070514be24b2980e7692fd99ebddbc965f43c6c9ba22cf0d7f9091bf03f44661298c69a6cf98eec5778a4eaf17a9d93b5da1'
